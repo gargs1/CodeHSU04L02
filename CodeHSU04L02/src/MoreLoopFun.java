0f766be126ee89f2a8da4c0cfbc60bf54b807b3c
gargs1 <gargs@wlhs.wlwv.k12.or.us>
2022-12-08 09:43:28 -0800

    Completed for and while loops.

--- CodeHSU04L02/src/MoreLoopFun.java
+++ CodeHSU04L02/src/MoreLoopFun.java
@@ -7,17 +7,31 @@ public class MoreLoopFun {
          */
 
         //a for loop that counts by ones from 1 to 5
+        for(int start = 1; int < 5; start++) {
+            System.out.println(start + " ");
+        }
 
 
         //a for loop that prints the powers of two up to 100
         //can you have an infinite for loop?
+        System.out.println("Powers of 2");
+        for(int start = 2; int <= 100; start*=2) {
+            System.out.println(start + " ");
+        }
 
-
+        System.out.println("Counting by 4s");
         //write a for loop that counts by 4s from 5 up to 50
+        for(int i = 5; i <= 50; i += 4) {
+            System.out.println(start + " ");
+        }
 
 
         //write a for loop that sums the numbers from 1 to 10
         int total = 0;
+        for( int i = 1; i <= 10; in++) {
+            i += total;
+        }
+        System.out.println("The of the integer 1 through 10 is: " + total);
 
         ////////////////////////////////////////////////////
         //for loops compared to while loops////////////////
@@ -34,6 +48,16 @@ public class MoreLoopFun {
             num++;
         }
         System.out.println("Finished!");
+
+        String name = "Mr. Smith";
+        int j = 0;
+        for(j< name.length()) {
+            System.out.println(name.charAt(j));
+            j++;
+        }
+        for(int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
 
     }
 }
